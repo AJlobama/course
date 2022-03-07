9cbfa6f1ad986575f8df25708ded9e0f5b413240
Alexsander Chemerchev <78754262+AJlobama@users.noreply.github.com>
2022-03-07 12:48:24 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,23 @@
+// let nav = document.querySelector('.nav'); 
+// let scrollTop;
+
+// function addClassActive() {
+//    nav.classList.add('active')
+// }
+
+// function removeClassActive() {
+//     nav.classList.remove('active')
+// }
+
+// window.addEventListener('scroll', function() {
+//    if(scrollTop > 100 && !nav.classList.contains('active')) {
+   
+//     addClassActive();
+//     console.log('add')
+//    } else if(scrollTop < 100 && nav.classList.contains('active')) {
+//     removeClassActive()
+//     console.log('del')
+//    }
+  
+//     scrollTop = scrollY
+// });
